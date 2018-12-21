9e5178292758a28748211135acea4637964ff558
Patrick Finazzo <pfinazzoproduction@gmail.com>
2018-12-20 22:48:18 -0800

    remove comments

--- src/utilities/todoService.js
+++ src/utilities/todoService.js
@@ -1,10 +1,8 @@
 
 export const saveTodos = (todos) => {
-  // console.log('pushed to local Storage', todos);
   localStorage.setItem("todos", JSON.stringify([...new Set([...todos])]));
 }
 
 export const getTodos = () => {
-  // console.log('pulled from local Storage', JSON.parse(localStorage.getItem("todos")));
   return JSON.parse(localStorage.getItem("todos")) || [];
 }
